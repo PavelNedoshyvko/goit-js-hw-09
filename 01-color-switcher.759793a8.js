128dd02b22076ef90fdaee6b9ce0cd6b0b4dade8
PavelNedoshyvko <113437527+PavelNedoshyvko@users.noreply.github.com>
2023-08-15 16:33:55 +0000

    Deploying to gh-pages from @ PavelNedoshyvko/goit-js-hw-09@86194e3c46cd487d2e6c601348eee702d541711f 🚀

--- 01-color-switcher.759793a8.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.setAttribute("disabled","disabled")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}));let e=null;
-//# sourceMappingURL=01-color-switcher.759793a8.js.map
