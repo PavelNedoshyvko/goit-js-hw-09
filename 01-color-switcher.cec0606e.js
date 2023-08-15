063eaa29276ecbfb38c4e2f1436741ab97843955
PavelNedoshyvko <113437527+PavelNedoshyvko@users.noreply.github.com>
2023-08-15 10:06:14 +0000

    Deploying to gh-pages from @ PavelNedoshyvko/goit-js-hw-09@f674e83b0471d2f504b9ccc60cadd78cbda84670 🚀

---
+++ 01-color-switcher.cec0606e.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.startBtn.setAttribute("disabled","disabled")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}));var e=null}();
+//# sourceMappingURL=01-color-switcher.cec0606e.js.map
