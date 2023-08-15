f674e83b0471d2f504b9ccc60cadd78cbda84670
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-15 13:05:46 +0300

    added 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+	startBtn: document.querySelector('[data-start]'),
+	stopBtn: document.querySelector('[data-stop]'),
+}
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() { 
+	intervalId = setInterval(() => {
+		document.body.style.backgroundColor = getRandomHexColor();
+	}, 1000);
+	refs.startBtn.setAttribute("disabled", "disabled");
+};
+
+function onStopBtnClick() { 
+	clearInterval(intervalId);
+	refs.startBtn.removeAttribute("disabled");
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
